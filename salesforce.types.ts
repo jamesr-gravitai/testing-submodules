6af18311963a3f0a5c701d6a8dafdf51154812b7
jamesroberts <james@pushbar.co.uk>
2021-11-02 19:04:12 +0000

    james-test

--- salesforce.types.ts
+++ salesforce.types.ts
@@ -2,8 +2,5 @@ export enum Gender_Enum {
   Female = 'Female',
   Male = 'Male',
   DeclineToSpecify = 'Decline to Specify',
-  Other = 'Other',
-  New = 'New',
-  New1 = 'New1',
-  New2 = 'New2',
+  Other = 'Other'
 }
